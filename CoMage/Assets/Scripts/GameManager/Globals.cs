6d9e42c3ef06f8fdeece098efce20e0822dc8ab0
BitBinder <travisguitar062849@gmail.com>
2014-06-14 14:23:44 -0500

    More Changes to Entity

---
+++ CoMage/Assets/Scripts/GameManager/Globals.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GameManager
+{
+    public class Globals : MonoBehaviour
+    {
+        public List<Entity> gameEntites;
+        public static Globals instance;
+
+        void Awake()
+        {
+            instance = this;
+            gameEntites = new List<Entity>();
+        }
+    }
+}
